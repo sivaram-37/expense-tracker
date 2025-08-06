b95db5f4baa304c364b245a950f3d8bff37d3420
sivaram <sivaram3710@gmail.com>
2025-08-06 18:15:44 +0530

    increased the padding of the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Header />
           <div className="h-[calc(100dvh-64px)] transition-colors flex">
             <SideNavbar />
-            <div className="p-2 sm:p-4 w-full bg-background text-foreground overflow-auto">
+            <div className="p-3 sm:p-8 w-full bg-background text-foreground overflow-auto">
               {children}
             </div>
           </div>
