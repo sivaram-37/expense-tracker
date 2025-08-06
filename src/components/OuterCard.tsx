ee8f0ba0ad20b8794ae7f66d940194532d168c8c
sivaram <sivaram3710@gmail.com>
2025-08-06 16:48:11 +0530

    modified OuterCard to display includeHeader if the prop is passed

---
+++ src/components/OuterCard.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export const tabs = [
+  { id: "/dashboard", label: "Dashboard" },
+  { id: "/add-expense", label: "Add New Expense" },
+  { id: "/expenses", label: "Recent Expenses" },
+  { id: "/analytics", label: "Analytics" },
+  { id: "/budgets", label: "Budget Manager" },
+];
+
+export function getTabInfoById(id: string): { label: string } {
+  const tab = tabs.find((tab) => tab.id === id);
+  return { label: tab!.label };
+}
+
+const OuterCard = ({
+  children,
+  className = "",
+  includeHeader = false,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  includeHeader?: boolean;
+}) => {
+  const path = usePathname();
+  const { label } = getTabInfoById(path);
+
+  return (
+    <div
+      className={cn(
+        "p-2 sm:p-6 bg-gray-100 dark:bg-gray-900 rounded-xl border border-gray-200 dark:border-gray-800",
+        includeHeader ? "pt-0 sm:pt-0" : "",
+        className
+      )}>
+      {includeHeader && (
+        <h2 className="sticky top-[-16px] z-50 bg-gray-100 dark:bg-gray-900 text-lg lg:text-2xl font-extrabold text-gray-900 dark:text-white py-3 lg:py-6 flex items-center justify-center gap-2">
+          <span className="mb-2 border-b-6 border-b-emerald-500 border-double">{label}</span>
+        </h2>
+      )}
+      {children}
+    </div>
+  );
+};
+
+export default OuterCard;
--- src/layout/OuterCard.tsx
+++
@@ -1,9 +0,0 @@
-const OuterCard = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="bg-white dark:bg-gray-800 rounded-xl shadow-lg p-2 sm:p-6 border border-gray-100 dark:border-gray-700">
-      {children}
-    </div>
-  );
-};
-
-export default OuterCard;
