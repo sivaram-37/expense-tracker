100ea36b0b6eec98b7272ba9776a59f31bfd5843
sivaram <sivaram3710@gmail.com>
2025-08-07 17:37:44 +0530

    added new route - /settings

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { BarChart3, PlusCircle, List, Target, Home } from "lucide-react";
+import { BarChart3, PlusCircle, List, Target, Home, Settings } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useCallback } from "react";
 
 const tabs = [
   { id: "/dashboard", label: "Dashboard", icon: Home },
@@ -14,39 +13,47 @@ const tabs = [
   { id: "/budgets", label: "Budgets", icon: Target },
 ];
 
+const renderLink = (
+  id: string,
+  label: string,
+  Icon: React.ElementType,
+  isActive: boolean,
+  showLabel = true
+) => (
+  <Link
+    key={id}
+    href={id}
+    className={cn(
+      "w-full flex items-center gap-3 p-2 rounded-lg text-left transition-colors",
+      isActive
+        ? "bg-primary text-white"
+        : "text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700"
+    )}>
+    <Icon className="w-5 h-5" />
+    {showLabel && <span>{label}</span>}
+  </Link>
+);
+
 const SideNavbar = () => {
   const path = usePathname();
 
-  const renderNavigationLinks = useCallback(
-    (useLabelWithIcon = true) =>
-      tabs.map(({ id, label, icon: Icon }) => {
-        const isActive = path === id;
-        return (
-          <Link
-            key={id}
-            href={id}
-            className={cn(
-              "w-full flex items-center gap-3 p-2 rounded-lg text-left transition-colors",
-              isActive
-                ? "bg-primary text-white"
-                : "text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700"
-            )}>
-            <Icon className="w-5 h-5" />
-            {useLabelWithIcon && <span>{label}</span>}
-          </Link>
-        );
-      }),
-    [path]
-  );
-
   return (
     <>
-      <nav className="p-4 h-[calc(100dvh-64px)] sticky w-[300px] space-y-2 bg-sidebar-border hidden md:block">
-        {renderNavigationLinks()}
-      </nav>
-      <nav className="p-2 h-[calc(100dvh-64px)] sticky space-y-2 bg-sidebar-border block md:hidden">
-        {renderNavigationLinks(false)}
-      </nav>
+      {/* Desktop Sidebar */}
+      <div className="w-[210px] px-2 py-4 h-[calc(100dvh-64px)] sticky bg-sidebar-border hidden md:flex flex-col justify-between">
+        <nav className="space-y-3">
+          {tabs.map(({ id, label, icon }) => renderLink(id, label, icon, path === id))}
+        </nav>
+        <nav>{renderLink("/settings", "Settings", Settings, path === "/settings")}</nav>
+      </div>
+
+      {/* Mobile Sidebar */}
+      <div className="p-2 h-[calc(100dvh-64px)] sticky bg-sidebar-border flex flex-col justify-between md:hidden">
+        <nav className="space-y-2">
+          {tabs.map(({ id, label, icon }) => renderLink(id, label, icon, path === id, false))}
+        </nav>
+        <nav>{renderLink("/settings", "Settings", Settings, path === "/settings", false)}</nav>
+      </div>
     </>
   );
 };
